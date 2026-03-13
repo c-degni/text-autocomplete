30223b758c1f138c32e5fc068efdd938bf7dbe4a
c-degni <christivanndeg@gmail.com>
2026-03-13 11:42:51 -0400

    Case sensitive

--- src/dictionary/dictionary.ts
+++ src/dictionary/dictionary.ts
@@ -3,8 +3,6 @@ import { DEFAULT_WORDS, CS_WORDS } from './words'
 
 let DEFAULT_TRIE = new Trie();
 DEFAULT_WORDS.forEach(word => DEFAULT_TRIE.insert(word));
-
-// May later turn the following dictionaries as separate user-enabled subdictionaries
 CS_WORDS.forEach(word => DEFAULT_TRIE.insert(word));
 
 export { DEFAULT_TRIE };
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,17 @@
 import { Editor, EditorPosition } from 'obsidian';
 
+export interface Result {
+    ok: boolean;
+    reason?: string;
+};
+
+export const OK: Result = { ok: true };
+export const NOT_OK: Result = { ok: false };
+export const EMPTY_FILE: Result = { ok: false, reason: 'emptyFile' };
+export const DUPLICATE_FILE: Result = { ok: false, reason: 'duplicateFile' };
+export const EMPTY_WORD: Result = { ok: false, reason: 'emptyWord' };
+export const DUPLICATE_WORD: Result = { ok: false, reason: 'duplicateWord'};
+
 export const inCodeBlock = (editor: Editor, cursor: EditorPosition): boolean => {
     const lines = editor.getValue().split('\n');
     let inCodeBlock = false;
